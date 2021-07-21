5c166ca2efeea9b7a64c3ffd86c84d2e7753b873
priyadharshini149 <69582723+priyadharshini149@users.noreply.github.com>
2021-07-21 23:06:41 +0530

    file has been changed

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 function validation(){
     var name=document.getElementById('name').value;
-    var email=document.getElementById('email').value;
+    
     var number=document.getElementById('number').value;
 
     var usercheck=/^[A-Za-z]{3,30}$/;
-    var emailcheck=/^[A-Za-z_.0-9]{3,}@kongu.edu$/;
+    
     var numbercheck=/^[6789][0-9]{9}$/;
 
     if(usercheck.test(name))
@@ -15,17 +15,9 @@ function validation(){
     {
         document.getElementById('nameerror').innerHTML="pls enter the valid name,size(3-30)!!!";
         
-        return false;
+        validation();
     }
-    if(emailcheck.test(email))
-    {
-        document.getElementById('emailerror').innerHTML=" ";
-    }
-    else
-    {
-        document.getElementById('emailerror').innerHTML="pls use kongu mail id!!!";
-        return false;
-    }
+  
     if(numbercheck.test(number))
     {
         document.getElementById('numbererror').innerHTML=" ";
@@ -33,21 +25,37 @@ function validation(){
     else
     {
         document.getElementById('numbererror').innerHTML="pls enter the valid phone number!!!";
-        return false;
+        validation();
     }
 
-
+    
 
  
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwEMB__wJWHfrWSP2C_vjjzxFiJDBYAgVQp-zg-ivKyeI8zCo2qc49-BzDJIS204GPH/exec'
-const form = document.forms['register form']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyzCZ5Ba9crXtDNVQX8ZZCpTn6S58uO3jnDkv01MRYuEvGXuCJ-QueRxOb41F8I_Vk0kQ/exec'
+const form = document.forms['ecommerce']
 
-form.addEventListener('submit', e => {
+
+
+form.addEventListener('submit', 
+
+
+ e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for registering"))
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)},validation()) 
+  
+  .then(response => alert("THanks FoR RegIsterIng!!!"))
     .catch(error => console.error('Error!', error.message))
+ 
+    
+    
+   
+
+   
 })
+
+
+
+
 
